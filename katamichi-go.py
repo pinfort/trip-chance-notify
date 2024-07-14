4514087edae03d746d29ff921e32d0f4e1a411f5
pinfort <ptg@nijitei.com>
2024-07-14 22:37:02 +0900

    update py

--- katamichi-go.py
+++ katamichi-go.py
@@ -12,21 +12,22 @@ class Car:
     date: str
     car_type: str
     condition: str
-    info_date: str
     reserve: str
     expired: bool
 
+def getInformationFromClassWithoutLabel(soup, class_: str) -> str:
+    return soup.find(class_=class_).find(class_="label-sp").find_next_sibling().get_text(strip=True)
+
 def getCar(car) -> Car:
-    shop_start = getTextFromClassName(car, "service-item__shop-start")
-    shop_return = getTextFromClassName(car, "service-item__shop-return")
-    date = getTextFromClassName(car, "service-item__date")
-    car_type = getTextFromClassName(car, "service-item__info__car-type")
-    condition = getTextFromClassName(car, "service-item__info__condition")
-    info_date = getTextFromClassName(car, "service-item__info__date")
-    reserve = getTextFromClassName(car, "service-item__reserve")
+    shop_start = getInformationFromClassWithoutLabel(car, "service-item__shop-start")
+    shop_return = getInformationFromClassWithoutLabel(car, "service-item__shop-return")
+    date = getInformationFromClassWithoutLabel(car, "service-item__date")
+    car_type = getInformationFromClassWithoutLabel(car, "service-item__info__car-type")
+    condition = getInformationFromClassWithoutLabel(car, "service-item__info__condition")
+    reserve = getInformationFromClassWithoutLabel(car, "service-item__reserve")
     expired = 'show-entry-end' in car.find(class_="service-item__body")['class']
     
-    return Car(shop_start, shop_return, date, car_type, condition, info_date, reserve, expired)
+    return Car(shop_start, shop_return, date, car_type, condition, reserve, expired)
 
 cars: list[Car] = []
 
